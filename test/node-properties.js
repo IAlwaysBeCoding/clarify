c0cd0b244cd2c31882ac74e4c79543e64f861232
Erik Dominguez <IAlwaysBeCoding@users.noreply.github.com>
2018-12-13 15:53:15 -0800

    remove nodes.js from test, and transfer the helper functions over to node-properties.js

--- test/node-properties.js
+++ test/node-properties.js
@@ -1,8 +1,75 @@
-/* global describe it */
+/* global describe it __dirname*/
 
+
+const path = require("path");
+const fs = require("fs");
+const parse = require("esprima").parse;
+const visit = require("ast-types").visit;
 const assert = require("assert");
 const props = require("../src/node-properties.js");
-const nodes = require("./nodes.js");
+const simpleNodes = require("./nodes/index.js");
+
+
+const nodeNames = ["Program", "EmptyStatement", "BlockStatement", "ExpressionStatement",
+  "IfStatement", "LabeledStatement", "BreakStatement", "ContinueStatement", "WithStatement",
+  "SwitchStatement", "ReturnStatement", "ThrowStatement", "TryStatement", "CatchClause",
+  "WhileStatement", "DoWhileStatement", "ForStatement", "ForInStatement", "DebuggerStatement",
+  "FunctionDeclaration", "FunctionExpression", "VariableDeclaration", "VariableDeclarator",
+  "ThisExpression", "ArrayExpression", "ObjectExpression", "SequenceExpression", 
+  "UnaryExpression", "BinaryExpression", "AssignmentExpression", "UpdateExpression",
+  "LogicalExpression", "ConditionalExpression", "NewExpression", "CallExpression", 
+  "MemberExpression", "SwitchCase", "Identifier", "Literal"
+];
+
+const getNode = (name) => {
+
+  if (!simpleNodes.hasOwnProperty(name)){
+    throw Error("Node name: "+ name+ " does not exist!");
+  }
+
+  //Load the source code for each Node type
+  const source = fs.readFileSync(
+    path.join(__dirname, "nodes", name+".js"),
+    "utf-8"
+  );
+
+  //Parse the source code and produce an AST node
+  const ast = parse(source);
+
+  //Set the node to null before traversing through the AST to find the
+  //first occurence of the node name we are looking for
+  var node = null;
+	
+  visit(ast, {
+    visitNode: function(path) {
+      //Return the first instance of the node name we are looking for
+      if(path.value.type == name){
+        node = path.node;
+        this.abort();
+      }
+      this.traverse(path);
+    }
+  });
+
+  if(node == null){
+    console.log("Name:", name, " Return:", node);
+  }
+
+  return node;
+
+};
+
+const getAllNodes = () => {
+	
+  const nodes = [];
+
+  //Iterate through all the possible node names
+  nodeNames.map((name) => {
+    nodes.push(getNode(name));
+  });
+	
+  return nodes;
+};
 
 
 describe("Checking nodes basic properties.", function(){
@@ -10,7 +77,7 @@ describe("Checking nodes basic properties.", function(){
     it("Should contain only nodes with a body block.", function(){
 
       const nodez = [];
-      const _nodes = nodes.getAllNodes();
+      const _nodes = getAllNodes();
 
       //Let's iterate through all of the possible nodes
       _nodes.map((n) => {
@@ -34,7 +101,7 @@ describe("Checking nodes basic properties.", function(){
     it("Should contain only nodes that have a \"right\" and a \"left\" leaf node.", function(){
 
       const nodez = [];
-      const _nodes = nodes.getAllNodes();
+      const _nodes = getAllNodes();
 
       //Let's iterate through all of the possible nodes
       _nodes.map((n) => {
@@ -63,7 +130,7 @@ describe("Checking nodes basic properties.", function(){
     it("Should contain only nodes that have a \"right\" and a \"left\" leaf node excluding the ForInStatement node.", function(){
 
       const nodez = [];
-      const _nodes = nodes.getAllNodes();
+      const _nodes = getAllNodes();
 
       //Let's iterate through all of the possible nodes
       _nodes.map((n) => {
@@ -94,7 +161,7 @@ describe("Checking nodes basic properties.", function(){
     it("Should contain only nodes that have a \"callee\" property", function(){
 
       const nodez = [];
-      const _nodes = nodes.getAllNodes();
+      const _nodes = getAllNodes();
 
       //Let's iterate through all of the possible nodes
       _nodes.map((n) => {
